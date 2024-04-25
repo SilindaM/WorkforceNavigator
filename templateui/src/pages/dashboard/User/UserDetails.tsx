ac17ba320cc9c95f0c08d3b55e7eea1988c5a60c
silindaM <silindanemdu@gmail.com>
2024-04-25 23:00:49 +0200

    fixed frontend code

--- templateui/src/pages/dashboard/User/UserDetails.tsx
+++ templateui/src/pages/dashboard/User/UserDetails.tsx
@@ -10,6 +10,7 @@ import {
   Table,
   TableBody,
   TableRow,
+  Divider,
 } from "semantic-ui-react";
 import { UserDetailsDto } from "../../../types/userDetails.type";
 import axiosInstance from "../../../utils/axiosInstance";
@@ -70,100 +71,169 @@ const UserDetails = ({ username }: IProps) => {
   return (
     <div style={{ padding: "20px" }}>
       <Segment>
-        <List relaxed>
-          <List.Item>
-            <Segment>
-              <Grid columns={2}>
-                <Grid.Column width={3}>
-                  <Icon name="user circle" size="big" />
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  <Header as="h3">Personal Information</Header>
-                  <List.Description>
-                    <p>
-                      <strong>First Name:</strong> {userDetails?.firstName}
-                    </p>
-                    <p>
-                      <strong>Last Name:</strong> {userDetails?.lastName}
-                    </p>
-                    <p>
-                      <strong>Email :</strong> {userDetails?.email}
-                    </p>
-                    <p>
-                      <strong>Gender:</strong> {userDetails?.gender}
-                    </p>
-                  </List.Description>
-                </Grid.Column>
-              </Grid>
-            </Segment>
-          </List.Item>
-          <List.Item>
-            <Segment>
-              <Grid columns={2}>
-                <Grid.Column width={3}>
-                  <Icon name="briefcase" size="big" />
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  <Header as="h3">Employment Details</Header>
-                  <List.Description>
-                    <p>
-                      <strong>Job Title:</strong> {userDetails?.jobTitle}
-                    </p>
-                    <p>
-                      <strong>Salary:</strong> {userDetails?.salary}
-                    </p>
-                    <p>
-                      <strong>Line Manager:</strong> {userDetails?.lineManager}
-                    </p>
-                    <p>
-                      <strong>Department:</strong> {userDetails?.department}
-                    </p>
-                    <p>
-                      <strong>Job Level:</strong> {userDetails?.seniority}
-                    </p>
-                  </List.Description>
-                </Grid.Column>
-              </Grid>
-            </Segment>
-          </List.Item>
+        <div className="ui form">
+          <div className="two fields">
+            <div className="field ">
+              <>
+                <label>First Name</label>
+                <input type="text" value={userDetails?.firstName}></input>
+              </>
+            </div>
+            <div className="field">
+              <>
+                <label>Last Name</label>
+                <input type="text" value={userDetails?.lastName}></input>
+              </>
+            </div>
+            <div className="field ">
+              <>
+                <label>Gender</label>
+                <input type="text" value={userDetails?.gender} disabled></input>
+              </>
+            </div>
+          </div>
+          <div className="ui inverted divider"></div>
+          <div className="two fields mt-2">
+            <div className="field ">
+              <>
+                <label>User Name</label>
+                <input type="text" value={userDetails?.username}></input>
+              </>
+            </div>
+            <div className="field">
+              <>
+                <label>Email</label>
+                <input type="text" value={userDetails?.email}></input>
+              </>
+            </div>
+            <div className="field ">
+              <>
+                <label>Cellphone</label>
+                <input type="text" value={userDetails?.email}></input>
+              </>
+            </div>
+          </div>
+          <div className="two fields"></div>
+          <div className="ui inverted divider"></div>
+          <div className="two fields mt-2">
+            <div className="field ">
+              <>
+                <label>Career Manager</label>
+                <input
+                  placeholder="Career Manager"
+                  type="text"
+                  value={userDetails?.lineManager}
+                ></input>
+              </>
+            </div>
+            <div className="field">
+              <>
+                <label>Department</label>
+                <input
+                  placeholder="Last Name"
+                  type="text"
+                  value={userDetails?.department} disabled
+                ></input>
+              </>
+            </div>
+            <div className="field">
+              <>
+                <label>Level</label>
+                <input
+                  placeholder="First Name"
+                  type="text"
+                  value={userDetails?.seniority} disabled
+                ></input>
+              </>
+            </div>
+          </div>
+          <div className="ui inverted divider"></div>
+          <div className="two fields">
+            <div className="field">
+              <>
+                <label>Assigned Roles</label>
+                <input
+                  placeholder="Last Name"
+                  type="text"
+                  value={userDetails?.roles} disabled
+                ></input>
+              </>
+            </div>
+            <div className="field">
+              <>
+                <label>Joining Date</label>
+                <input
+                  placeholder="Joining Date"
+                  type="date"
+                  value={
+                    userDetails?.joiningDate
+                      ? new Date(userDetails.joiningDate)
+                          .toISOString()
+                          .substring(0, 10)
+                      : ""
+                  } disabled
+                ></input>
+              </>
+            </div>
 
-          <Table size="small" bordered>
-            <Table.Header>
-              <Table.Header>UPCOMING LEAVES</Table.Header>
+            <div className="field">
+              <>
+                <label>Job Title</label>
+                <input
+                  placeholder="Last Name"
+                  type="text"
+                  value={userDetails?.jobTitle}
+                ></input>
+              </>
+            </div>
+          </div>
+          <Button primary>Update User Details</Button>
+        </div>
+        <Divider horizontal>Upcoming Leaves</Divider>
+        <Table size="small" bordered>
+          <Table.Header>
+            <TableRow>
+              <Table.HeaderCell>Start Date</Table.HeaderCell>
+              <Table.HeaderCell>End Date</Table.HeaderCell>
+              <Table.HeaderCell>Requested Date</Table.HeaderCell>
+              <Table.HeaderCell>Number Of Days</Table.HeaderCell>
+              <Table.HeaderCell>Leave Name</Table.HeaderCell>
+              <Table.HeaderCell>Status</Table.HeaderCell>
+            </TableRow>
+          </Table.Header>
+          <TableBody>
+            {leaves.filter((row) => row.status === Status.Approved).length >
+            0 ? (
+              // Render approved leave requests if there are any
+              leaves
+                .filter((row) => row.status === Status.Approved)
+                .map((row) => (
+                  <TableRow key={row.id}>
+                    {/* Table cells */}
+                    <Table.Cell>
+                      {new Date(row.startDate).toLocaleDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {new Date(row.endDate).toLocaleDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {new Date(row.requestedDate).toLocaleDateString()}
+                    </Table.Cell>
+                    <Table.Cell>{row.numberOfDays}</Table.Cell>
+                    <Table.Cell>{row.leaveName}</Table.Cell>
+                    <Table.Cell>{row.status}</Table.Cell>
+                  </TableRow>
+                ))
+            ) : (
+              // Render "No upcoming leave requests" if there are no approved leaves
               <TableRow>
-                <Table.HeaderCell>Start Date</Table.HeaderCell>
-                <Table.HeaderCell>End Date</Table.HeaderCell>
-                <Table.HeaderCell>Requested Date</Table.HeaderCell>
-                <Table.HeaderCell>Number Of Days</Table.HeaderCell>
-                <Table.HeaderCell>Leave Name</Table.HeaderCell>
-                <Table.HeaderCell>Status</Table.HeaderCell>
+                <Table.Cell colSpan={6} textAlign="center">
+                  No upcoming leave requests
+                </Table.Cell>
               </TableRow>
-            </Table.Header>
-            <TableBody>
-  {leaves.filter(row => row.status === Status.Approved).length > 0 ? (
-    // Render approved leave requests if there are any
-    leaves.filter(row => row.status === Status.Approved).map(row => (
-      <TableRow key={row.id}>
-        {/* Table cells */}
-        <Table.Cell>{new Date(row.startDate).toLocaleDateString()}</Table.Cell>
-        <Table.Cell>{new Date(row.endDate).toLocaleDateString()}</Table.Cell>
-        <Table.Cell>{new Date(row.requestedDate).toLocaleDateString()}</Table.Cell>
-        <Table.Cell>{row.numberOfDays}</Table.Cell>
-        <Table.Cell>{row.leaveName}</Table.Cell>
-        <Table.Cell>{row.status}</Table.Cell>
-      </TableRow>
-    ))
-  ) : (
-    // Render "No upcoming leave requests" if there are no approved leaves
-    <TableRow>
-      <Table.Cell colSpan={6} textAlign="center">No upcoming leave requests</Table.Cell>
-    </TableRow>
-  )}
-</TableBody>
-
-
-          </Table>
-        </List>
+            )}
+          </TableBody>
+        </Table>
       </Segment>
     </div>
   );
