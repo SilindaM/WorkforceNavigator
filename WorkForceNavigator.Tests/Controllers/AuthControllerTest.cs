79ccc4f09326e1e01fdacfd66bbd97a3e1fc2ea7
silindaM <silindanemdu@gmail.com>
2024-04-30 15:52:31 +0200

    Adding Unit tests

---
+++ WorkForceNavigator.Tests/Controllers/AuthControllerTest.cs
@@ -0,0 +1,32 @@
+﻿//namespace WorkForceNavigator.Tests.Controllers
+//{
+//  using Application.Interfaces.Auth;
+//  using Application.Services.Auth;
+//  using AutoMapper;
+//  using Domain.Dtos.Account;
+//  using FakeItEasy;
+//  using System;
+
+//  public class AuthControllerTest
+//  {
+//    private readonly IAuthService authService;
+//    private readonly IMapper mapper;
+
+//    public AuthControllerTest()
+//    {
+//      authService = A.Fake<IAuthService>();
+//      mapper = A.Fake<IMapper>();
+//    }
+//    [Fact]
+//    [Theory]
+//    public void AuthController_Login_Return_Ok()
+//    {
+//      //Arrange 
+//      var  login = A.Fake<LoginDto>();
+      
+//      //Act
+//      //Assert
+//    }
+
+//  }
+//}
