dd7e9028464bf8ff449a0995f3613f982093459a
silindaM <silindanemdu@gmail.com>
2024-05-02 09:36:24 +0200

    timesheets related models

--- Domain/Enties/Department.cs
+++ Domain/Enties/Department.cs
@@ -1,16 +1,16 @@
-﻿using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Enties
+﻿namespace Domain.Enties
 {
-    public class Department : BaseEntity<int>
-    {
-        public string DepartmentName { get; set; }
-        public string Description { get; set; }
+  using Domain.Entities;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class Department : BaseEntity<int>
+  {
+    public string DepartmentName { get; set; }
+    public string Description { get; set; }
     // Navigation property to access the associated job titles
     public ICollection<JobTitle> JobTitles { get; set; }
   }
---
+++ Domain/Enties/TimeSheets/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Enties.TimeSheets
+{
+  using Domain.Entities;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class Client : BaseEntity<int>
+  {
+    public string ClientName { get; set; }
+    public string Phone { get; set; }
+    public string Fax { get; set; }
+    public string Email { get; set; }
+  }
+}
