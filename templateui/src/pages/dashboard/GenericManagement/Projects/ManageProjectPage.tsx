7abea0d20e5f1db34486f3fd5d6a46a7f866a664
silindaM <silindanemdu@gmail.com>
2024-06-05 19:51:53 +0200

    complete project details

--- templateui/src/pages/dashboard/GenericManagement/Projects/ManageProjectPage.tsx
+++ templateui/src/pages/dashboard/GenericManagement/Projects/ManageProjectPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Grid, Segment, Container, Header, GridColumn } from "semantic-ui-react";
 import ProjectsPage from "./ProjectsPage";
-import ProjectDetails from "./ProjectDetailsPage";
 
 const ManageProjectPage = () => {
   const [selectedProjectId, setSelectedProjectId] = useState<number | null>(null);
@@ -13,20 +12,12 @@ const ManageProjectPage = () => {
   return (
     <Container fluid className="pageTemplate3">
       <Grid columns={2}>
-        <GridColumn width={8}>
+        <GridColumn width={16}>
           <Segment raised>
             <Header as="h2" textAlign="center">
               PROJECTS
             </Header>
             <ProjectsPage selectedProjectId={handleProjectSelect} />
-          </Segment>
-        </GridColumn>
-        <GridColumn width={8}>
-          <Segment raised>
-            <Header as="h2" textAlign="center">
-              PROJECT DETAILS
-            </Header>
-            {selectedProjectId && <ProjectDetails selectedProject={selectedProjectId} />}
           </Segment>
         </GridColumn>
       </Grid>
